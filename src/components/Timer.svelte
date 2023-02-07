2a7a35cdd0d0f8f248fb8d43258b1e57102bfe52
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-02-07 16:44:42 -0500

    Update Timer.svelte

--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -6,7 +6,27 @@
 
 
 
-<p>Timer</p>
+<style>
+.start {
+    background-color: rgb(rgb(236, 89, 89)(87, 43, 43), green, blue);
+    width: 100%;
+    margin: 10px 0;
+}    
 
+</style>
+
+<div bp="grid">
+
+    <h2 bp="offset-5@md 4@md 12@sm">
+
+        Seconds Left:
+
+    </h2>
+
+
+</div>
 
 <ProgressBar />
+
+
+<button class="start">Start</button>
